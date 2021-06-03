dd22a33c63aba9cde121a6c216f9d99823a42dbe
윤성혁 <yun2615928@ajou.ac.kr>
2021-06-03 14:02:28 +0900

    almost finish

--- Test.py
+++ Test.py
@@ -59,8 +59,8 @@ approx = cv2.approxPolyDP(big_contour,200,True)
 #print(contour2)
 #print(hierarchy)
 
-#final = cv2.drawContours(img, big_contour, -1, (0, 255, 0), 3)
-#final = cv2.drawContours(img, [approx], -1, (0, 255, 0), 3)
+final = cv2.drawContours(img, big_contour, -1, (0, 255, 0), 3)
+final = cv2.drawContours(img, [approx], -1, (0, 255, 0), 3)
 contour_img = cv2.drawContours(img_tmp, [approx], -1, (0, 255, 0), 3)
 imgray = cv2.cvtColor(img_tmp,cv2.COLOR_BGR2GRAY)
 ret,thr = cv2.threshold(imgray,127,255,0)
@@ -69,21 +69,85 @@ ret,thr = cv2.threshold(imgray,127,255,0)
 # 해리스 코너 검출 ---①
 corner = cv2.cornerHarris(imgray, 2, 3, 0.04)
 # 변화량 결과의 최대값 10% 이상의 좌표 구하기 ---②
-coord = np.where(corner > 0.5* corner.max())
+coord = np.where(corner > 0.3* corner.max())
 coord = np.stack((coord[1], coord[0]), axis=-1)
 print(coord)
-
-"""
-#코너 값 비슷하면 삭제
-i = 1
+#corner1
+corner_tmp1 = []
+i = 0
 for x, y in coord:
-    if coord[i].x == coord[i-1].y:
-        #delete
-"""
+    corner_tmp1.append(x+y)
+    i=i+1
+min_k = 1200
+min_i = 0
+i = 0
+for k in corner_tmp1:
+    if k<min_k:
+        min_k=k
+        min_i=i
+    i=i+1
+corner1 = coord[min_i]
+#corner2
+corner_tmp2 = []
+i = 0
+for x, y in coord:
+    corner_tmp2.append((600-x)+y)
+    i=i+1
+print(corner_tmp2)
+min_k = 1200
+min_i = 0
+i = 0
+for k in corner_tmp2:
+    #print(k)
+    #print(min_k)
+    if k<min_k:
+        min_k=k
+        min_i=i
+    i=i+1
+corner3 = coord[min_i]
+#corner3
+corner_tmp3 = []
+i = 0
+for x, y in coord:
+    corner_tmp3.append(x+(600-y))
+    i=i+1
+print(corner_tmp3)
+min_k = 1200
+min_i = 0
+i = 0
+for k in corner_tmp3:
+    if k<min_k:
+        min_k=k
+        min_i=i
+        #print(min_k)
+        #print(min_i)
+    i=i+1
+corner2 = coord[min_i]
+#corner4
+corner_tmp4 = []
+i = 0
+for x, y in coord:
+    corner_tmp4.append((600-x)+(600-y))
+    i=i+1
+min_k = 1200
+min_i = 0
+i = 0
+for k in corner_tmp4:
+    if k<min_k:
+        min_k=k
+        min_i=i
+    i=i+1
+corner4 = coord[min_i]
+
+print(corner1)
+print(corner2)
+print(corner3)
+print(corner4)
+
 #print(coord[1])
 #모서리 길이 계산
-VerticalLength = coord[1][1]-coord[0][1]
-HorizontalLength = coord[2][0]-coord[1][0]
+VerticalLength = corner2[1]-corner1[1]
+HorizontalLength = corner4[0]-corner2[0]
 print(VerticalLength)
 print(HorizontalLength)
 
@@ -94,7 +158,86 @@ for x, y in coord:
 
 cv2.imshow('final', imgray)
 contour2, y = cv2.findContours(thr, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
-final4 = cv2.drawContours(img, contour2, -1, (0, 255, 0), 3)
+#final4 = cv2.drawContours(img, contour2, -1, (0, 255, 0), 3)
 #cv2.imshow('final2', final)
-#cv2.imshow('final4', final4)
+cv2.imshow('final4', final)
 cv2.waitKey(0)
+
+"""
+import cv2
+import numpy as np
+import utlis
+
+pathImage = 'testIMG6.jpeg'
+heightImg = 640
+widthImg  = 480
+
+utlis.initializeTrackbars()
+count=0
+
+while (1):
+
+    img = cv2.imread(pathImage)
+    img = cv2.resize(img, (widthImg, heightImg)) # RESIZE IMAGE
+    imgBlank = np.zeros((heightImg,widthImg, 3), np.uint8) # CREATE A BLANK IMAGE FOR TESTING DEBUGING IF REQUIRED
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # CONVERT IMAGE TO GRAY SCALE
+    imgBlur = cv2.GaussianBlur(imgGray, (5, 5), 1) # ADD GAUSSIAN BLUR
+    thres=utlis.valTrackbars() # GET TRACK BAR VALUES FOR THRESHOLDS
+    imgThreshold = cv2.Canny(imgBlur,thres[0],thres[1]) # APPLY CANNY BLUR
+    kernel = np.ones((5, 5))
+    imgDial = cv2.dilate(imgThreshold, kernel, iterations=2) # APPLY DILATION
+    imgThreshold = cv2.erode(imgDial, kernel, iterations=1)  # APPLY EROSION
+
+    ## FIND ALL COUNTOURS
+    imgContours = img.copy() # COPY IMAGE FOR DISPLAY PURPOSES
+    imgBigContour = img.copy() # COPY IMAGE FOR DISPLAY PURPOSES
+    contours, hierarchy = cv2.findContours(imgThreshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) # FIND ALL CONTOURS
+    cv2.drawContours(imgContours, contours, -1, (0, 255, 0), 10) # DRAW ALL DETECTED CONTOURS
+
+    # FIND THE BIGGEST COUNTOUR
+    biggest, maxArea = utlis.biggestContour(contours) # FIND THE BIGGEST CONTOUR
+    if biggest.size != 0:
+        biggest=utlis.reorder(biggest)
+        cv2.drawContours(imgBigContour, biggest, -1, (0, 255, 0), 20) # DRAW THE BIGGEST CONTOUR
+        imgBigContour = utlis.drawRectangle(imgBigContour,biggest,2)
+        pts1 = np.float32(biggest) # PREPARE POINTS FOR WARP
+        pts2 = np.float32([[0, 0],[widthImg, 0], [0, heightImg],[widthImg, heightImg]]) # PREPARE POINTS FOR WARP
+        matrix = cv2.getPerspectiveTransform(pts1, pts2)
+        imgWarpColored = cv2.warpPerspective(img, matrix, (widthImg, heightImg))
+
+        #REMOVE 20 PIXELS FORM EACH SIDE
+        imgWarpColored=imgWarpColored[20:imgWarpColored.shape[0] - 20, 20:imgWarpColored.shape[1] - 20]
+        imgWarpColored = cv2.resize(imgWarpColored,(widthImg,heightImg))
+
+        # APPLY ADAPTIVE THRESHOLD
+        imgWarpGray = cv2.cvtColor(imgWarpColored,cv2.COLOR_BGR2GRAY)
+        imgAdaptiveThre = cv2.adaptiveThreshold(imgWarpGray, 255, 1, 1, 7, 2)
+        imgAdaptiveThre = cv2.bitwise_not(imgAdaptiveThre)
+        imgAdaptiveThre = cv2.medianBlur(imgAdaptiveThre,3)
+
+        # Image Array for Display
+        imageArray = ([img,imgGray,imgThreshold,imgContours],
+                      [imgBigContour,imgWarpColored, imgWarpGray,imgAdaptiveThre])
+
+    else:
+        imageArray = ([img,imgGray,imgThreshold,imgContours],
+                      [imgBlank, imgBlank, imgBlank, imgBlank])
+
+    # LABELS FOR DISPLAY
+    lables = [["Original","Gray","Threshold","Contours"],
+              ["Biggest Contour","Warp Prespective","Warp Gray","Adaptive Threshold"]]
+
+    stackedImage = utlis.stackImages(imageArray,0.75,lables)
+    cv2.imshow("Result",stackedImage)
+
+    # SAVE IMAGE WHEN 's' key is pressed
+    if cv2.waitKey(1) & 0xFF == ord('s'):
+        cv2.imwrite("Scanned/myImage"+str(count)+".jpg",imgWarpColored)
+        cv2.rectangle(stackedImage, ((int(stackedImage.shape[1] / 2) - 230), int(stackedImage.shape[0] / 2) + 50),
+                      (1100, 350), (0, 255, 0), cv2.FILLED)
+        cv2.putText(stackedImage, "Scan Saved", (int(stackedImage.shape[1] / 2) - 200, int(stackedImage.shape[0] / 2)),
+                    cv2.FONT_HERSHEY_DUPLEX, 3, (0, 0, 255), 5, cv2.LINE_AA)
+        cv2.imshow('Result', stackedImage)
+        cv2.waitKey(300)
+        count += 1
+"""
