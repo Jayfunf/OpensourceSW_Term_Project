5567f9b2232c712c09cd393f6ebdcc94d35e72ea
윤성혁 <yun2615928@ajou.ac.kr>
2021-06-03 18:13:40 +0900

    add

--- calcul.py
+++ calcul.py
@@ -1,10 +1,10 @@
-horizen_t = 0 #
-MN = 0 #
-H = 0 #
-R = 0 #
+horizen_t = 574 #
+MN = 301 #
+H = 55 #
+R = 95 #
 
 pi = 3.1416
-rate_hv = 1.414
+rate_hv = 1/1.414
 horizen_actual = 297 #mm
 vertical_t = rate_hv*horizen_t
 rate_actual = horizen_actual/horizen_t
@@ -14,3 +14,4 @@ h_t = H/sin
 H_actual = h_t*rate_actual
 R_actual = R*rate_actual
 volume = R_actual^2 * pi * H_actual
+#322505
